9291f7109beca61e55716138a130fce0ea0d821c
Christophe1997 <sdl.office.1997@gmail.com>
2018-12-13 19:27:41 +0800

    add gitattributes for crlf and lf

---
+++ src/test/java/chris/se/SeApplicationTests.java
@@ -0,0 +1,17 @@
+package chris.se;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
